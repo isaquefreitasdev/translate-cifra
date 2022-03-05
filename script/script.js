659a9c60609403cc51d7fb1ea74ca2d4d73c48a1
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2022-03-05 18:00:07 -0300

    Version 3 - Bemois

--- script/script.js
+++ script/script.js
@@ -41,6 +41,23 @@ function calcNot(){
     }
     else if(valueCi.value === 'B#' || valueCi.value === 'b#'){
         res.innerHTML = 'Dó'
+    }else if(valueCi.value === 'Db' || valueCi.value === 'db'){
+        res.innerHTML = 'Ré Bemol'
+    }else if(valueCi.value === 'Eb' || valueCi.value === 'Eb'){
+        res.innerHTML = 'Mi Bemol'
+    }else if(valueCi.value === 'Gb' || valueCi.value === 'gb'){
+        res.innerHTML = 'Sol Bemol';
+    }else if(valueCi.value === 'Fb' || valueCi.value === 'fb'){
+        res.innerHTML = 'Fá Bemol'
+    }
+    else if(valueCi.value === 'Ab' || valueCi.value === 'ab'){
+        res.innerHTML = 'Lá Bemol'
+    }
+    else if(valueCi.value === 'Bb' || valueCi.value === 'bb'){
+        res.innerHTML = 'Si Bemol'
+    }
+    else if(valueCi.value === 'Cb' || valueCi.value === 'cb'){
+        res.innerHTML = 'Dó Bemol'
     }
     else{
         res.innerHTML = 'Error'
